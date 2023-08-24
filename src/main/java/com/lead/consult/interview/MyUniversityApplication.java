7d10266096ae61197e6dda2c2cd0afbcad098ca6
valentin <valentin7969@gmail.com>
2023-08-24 21:49:52 +0300

    Initial commit

---
+++ src/main/java/com/lead/consult/interview/MyUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.lead.consult.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUniversityApplication.class, args);
+	}
+
+}
